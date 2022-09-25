34f51352c6201024ab9927aa5eb94c3506d2031c
Morgane Mottey <morganemottey@MacBook-Pro-de-Morgane.local>
2022-09-25 22:04:20 +0200

    Remove background and test mesh position

--- webpack-version/src/script.js
+++ webpack-version/src/script.js
@@ -7,6 +7,9 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+mesh.position.y = 2
+mesh.position.x = 5
+mesh.position.z = -4
 scene.add(mesh)
 
 // Sizes
